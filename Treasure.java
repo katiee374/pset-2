212bb3a06b050486c3ddce58e118ca15fbe4668c
Katelynn Emerson <ke101@wellesley.edu>
2024-06-27 11:04:18 -0400

    updated Treasure

---
+++ Treasure.java
@@ -0,0 +1,41 @@
+
+/**
+ * Instructions: Create the following class to represent a treasure or object 
+ * in your game. Give it a name, description, and value.
+ * 
+ * Additionally, add an ArrayList of treasures as an instance variable of Room
+ * and Actor. Write getter and setter methods for these variables, along with 
+ * a method that returns a String description of all the objects in an Actor/
+ * Room.
+ * 
+ * Write a description of class Treasure here.
+ *
+ * @author (your name)
+ * @version (a version number or a date)
+ */
+public class Treasure
+{
+    // instance variables - replace the example below with your own
+    private int x;
+
+    /**
+     * Constructor for objects of class Treasure
+     */
+    public Treasure()
+    {
+        // initialise instance variables
+        x = 0;
+    }
+
+    /**
+     * An example of a method - replace this comment with your own
+     *
+     * @param  y  a sample parameter for a method
+     * @return    the sum of x and y
+     */
+    public int sampleMethod(int y)
+    {
+        // put your code here
+        return x + y;
+    }
+}
